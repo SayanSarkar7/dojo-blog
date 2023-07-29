da7dad72e6d3b323bd32e773e5426854e357cf9a
SayanSarkar7 <sarkarsayan602@gmail.com>
2023-07-29 22:59:40 +0530

    Created a Custom Hook

--- src/Home.js
+++ src/Home.js
@@ -1,28 +1,21 @@
 import {useEffect, useState} from 'react'
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 const Home = () => {
     // let name='mario';
-    const [blogs, setBlogs] = useState(null);
     
       // const handleDelete=(id)=>{
       //   const newBlogs=blogs.filter(blog=>blog.id!==id);
       //   setBlogs(newBlogs)
       // }
 
-      useEffect(()=>{
-        // npx json-server --watch data/db.json --port 8000
-        fetch(' http://localhost:8000/blogs ') 
-        .then(res=>{
-          return res.json();
-        })
-        .then(data=>{
-          // console.log(data);
-          setBlogs(data);
-        })
-      },[]);
+    const { data: blogs,isPending,error} = useFetch(' http://localhost:8000/blogs ');
+      
 
     return ( 
         <div className="home">
+            {error && <div> {error} </div>}
+            {isPending && <div>Loading...</div>}          
             {blogs && <BlogList blogs={blogs}/>}
             
         </div>
